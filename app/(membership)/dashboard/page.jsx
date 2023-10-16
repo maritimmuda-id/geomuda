8a25a6e435cd4104c7610bce68f0c2e8c489f982
satriaberliano <sanozisb@gmail.com>
2023-10-16 10:59:18 +0700

    fix : font-size dan border-width

--- app/(membership)/dashboard/page.jsx
+++ app/(membership)/dashboard/page.jsx
@@ -10,20 +10,20 @@ const DashboardPage = () => {
         <NavLeft />
         <div className='px-10 py-16 basis-5/6 h-screen'>
           <div className='space-y-2 mb-14'>
-            <h1 className='text-4xl mb-6'>Dashboard</h1>
+            <h1 className='text-3xl mb-6'>Dashboard</h1>
             <p>Halo User,</p>
             <p>Selamat datang di dashboard</p>
           </div>
           <div className='grid grid-cols-3 gap-5'>
-            <div className='shadow-xl max-w-lg rounded-xl bg-[#F1F1F1] p-4 space-y-2'>
+            <div className='shadow-xl max-w-lg rounded-xl bg-[#F1F1F1] p-4 space-y-2 border-2'>
               <h2>Jumlah User</h2>
               <p>-</p>
             </div>
-            <div className='shadow-xl max-w-lg rounded-xl bg-[#F1F1F1] p-4 space-y-2'>
+            <div className='shadow-xl max-w-lg rounded-xl bg-[#F1F1F1] p-4 space-y-2 border-2'>
               <h2>Lowongan Pekerjaan</h2>
               <p>-</p>
             </div>
-            <div className='shadow-xl max-w-lg rounded-xl bg-[#F1F1F1] p-4 space-y-2'>
+            <div className='shadow-xl max-w-lg rounded-xl bg-[#F1F1F1] p-4 space-y-2 border-2'>
               <h2>Jumlah User</h2>
               <p>-</p>
             </div>
