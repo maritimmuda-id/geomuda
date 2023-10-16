77205f3c4ff2f90f449e652bec7e57c24377da12
satriaberliano <sanozisb@gmail.com>
2023-10-16 10:57:41 +0700

    fix: height of nav

--- components/membership/NavMember/NavMember.jsx
+++ components/membership/NavMember/NavMember.jsx
@@ -3,7 +3,7 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 const NavMember = () => {
   return (
-    <div className='bg-[#7B2418] p-5 flex justify-between items-center px-16'>
+    <div className='bg-[#7B2418] p-5 flex justify-between items-center px-16 h-16'>
       <GiHamburgerMenu className='text-white text-2xl' />
       <button className='text-white'>User</button>
     </div>
