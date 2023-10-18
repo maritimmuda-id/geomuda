916c4a69ce6a34435657f150b602e9b9a32d5494
satriaberliano <sanozisb@gmail.com>
2023-10-18 08:50:19 +0700

    add : confirm password

--- components/membership/ChangePassword/ChangePassword.jsx
+++ components/membership/ChangePassword/ChangePassword.jsx
@@ -1,7 +1,7 @@
 const ChangePassword = () => {
   return (
     <div className='max-w-full rounded-xl bg-[#f8f8f8] p-10 min-h-full border-2 mt-6'>
-      <h2 className='text-xl font-semibold'>Ubah Password</h2>
+      <h2 className='text-xl font-semibold mb-6'>Ubah Password</h2>
       <form className="space-y-4 my-2">
         <div className='flex flex-col space-y-2'>
           <label>Password Lama</label>
@@ -9,6 +9,10 @@ const ChangePassword = () => {
         </div>
         <div className='flex flex-col space-y-2'>
           <label>Password Baru</label>
+          <input type='password' className='border-2 border-gray-500 active:border-[#7B2418] p-1 rounded-lg w-full'/>
+        </div>
+        <div className='flex flex-col space-y-2'>
+          <label>Konfirmasi Password Baru</label>
           <input type='password' className='border-2 border-gray-500 active:border-[#7B2418] p-1 rounded-lg w-full'/>
         </div>
         <button className='bg-[#7B2418] text-white p-2 rounded-lg w-full'>Simpan</button>
