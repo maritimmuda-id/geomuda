a66d4ffa2bcfdbd841af47226ca28082a3e2d1ad
satriaberliano <sanozisb@gmail.com>
2023-10-26 14:44:37 +0700

    add : modal

--- components/membership/ContentSidebar/ContentMember.jsx
+++ components/membership/ContentSidebar/ContentMember.jsx
@@ -5,9 +5,15 @@ import NavLeft from "../NavLeft/NavLeft";
 
 // ** Import Zustand
 import { useSidebar } from "@/zustand/useSidebar";
+import ModalEducation from "../ModalEducation/ModalEducation";
+import { useModalEducation } from "@/zustand/useModalEducation";
+import ModalExperienceWork from "../ModalExperienceWork/ModalExperienceWork";
+import { useModalExperience } from "@/zustand/useModalExperience";
 
 export default function ContentMember({ children }) {
   const { open } = useSidebar();
+  const { education } = useModalEducation();
+  const { experience } = useModalExperience();
 
   return (
     <div className="flex mt-10">
@@ -16,7 +22,12 @@ export default function ContentMember({ children }) {
           <NavLeft />
         </div>
       </div>
-
+      <div className={`fixed ${education ? "hidden" : "w-screen" }`}>
+        <ModalEducation />
+      </div>
+      <div className={`fixed ${experience ? "hidden" : "w-screen" }`}>
+        <ModalExperienceWork />
+      </div>
       <div className={`${open ? "pl-72" : ""} w-full`}>{children}</div>
     </div>
   );
