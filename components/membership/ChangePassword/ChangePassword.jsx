bb50bfb1fc033d430ba6168e34a0bc1f5e659734
satriaberliano <sanozisb@gmail.com>
2023-10-26 14:41:21 +0700

    add : padding

--- components/membership/ChangePassword/ChangePassword.jsx
+++ components/membership/ChangePassword/ChangePassword.jsx
@@ -5,15 +5,15 @@ const ChangePassword = () => {
       <form className="space-y-4 my-2">
         <div className='flex flex-col space-y-2'>
           <label>Password Lama</label>
-          <input type='password' className='border-2 border-gray-500 active:border-[#7B2418] p-1 rounded-lg w-full'/>
+          <input type='password' className='border-2 border-[#00000066] px-3 py-[6px] rounded-lg w-full'/>
         </div>
         <div className='flex flex-col space-y-2'>
           <label>Password Baru</label>
-          <input type='password' className='border-2 border-gray-500 active:border-[#7B2418] p-1 rounded-lg w-full'/>
+          <input type='password' className='border-2 border-[#00000066] px-3 py-[6px] rounded-lg w-full'/>
         </div>
         <div className='flex flex-col space-y-2'>
           <label>Konfirmasi Password Baru</label>
-          <input type='password' className='border-2 border-gray-500 active:border-[#7B2418] p-1 rounded-lg w-full'/>
+          <input type='password' className='border-2 border-[#00000066] px-3 py-[6px] rounded-lg w-full'/>
         </div>
         <button className='bg-[#7B2418] text-white p-2 rounded-lg w-full'>Simpan</button>
       </form>
