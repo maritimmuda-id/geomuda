d0b8949aa4e542aaef093fee6979e32ab7d48323
satriaberliano <sanozisb@gmail.com>
2023-10-30 11:50:47 +0700

    del : input old password

--- components/membership/ChangePassword/ChangePassword.jsx
+++ components/membership/ChangePassword/ChangePassword.jsx
@@ -1,12 +1,8 @@
 const ChangePassword = () => {
   return (
     <div className='max-w-full rounded-xl bg-[#f8f8f8] p-10 min-h-full border-2 mt-6'>
-      <h2 className='text-xl font-semibold mb-6'>Ubah Password</h2>
+      <h2 className='text-xl font-medium mb-6'>Ubah Password</h2>
       <form className="space-y-4 my-2">
-        <div className='flex flex-col space-y-2'>
-          <label>Password Lama</label>
-          <input type='password' className='border-2 border-[#00000066] px-3 py-[6px] rounded-lg w-full'/>
-        </div>
         <div className='flex flex-col space-y-2'>
           <label>Password Baru</label>
           <input type='password' className='border-2 border-[#00000066] px-3 py-[6px] rounded-lg w-full'/>
