2a627c03c0e196311917430da978e8f41a7ccc33
satriaberliano <sanozisb@gmail.com>
2023-11-27 16:22:03 +0700

    fix : image size

--- components/membership/TabsContent/Umum.jsx
+++ components/membership/TabsContent/Umum.jsx
@@ -98,7 +98,7 @@ export default function Umum({ countryData, user, provinceData }) {
           <div className="flex flex-col space-y-4">
             <div className="relative z-[1]">
               <Image
-                className="w-48 aspect-[3/4] rounded-xl border-2 border-slate-200 object-cover object-center"
+                className="w-40 aspect-[3/4] rounded-xl border-2 border-slate-200 object-cover object-center"
                 src={picture ? picture : assets.defaultImage}
                 width={500}
                 height={500}
