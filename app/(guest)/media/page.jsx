8aa883b08d4de4cf6818a5ca9de7d3d68a8a0092
satriaberliano <sanozisb@gmail.com>
2023-12-04 14:37:47 +0700

    fix : responsive interface

--- app/(guest)/media/page.jsx
+++ app/(guest)/media/page.jsx
@@ -12,26 +12,28 @@ const Media = () => {
         title="MEDIA GEOMUDA"
         subtitle="Asosiasi Geomuda Indonesia"
       />
-      <div className='flex space-x-12 text-white -mt-20 ml-24'>
-        <p>Beranda</p>
-        <p>Berita</p>
-        <p>Pengumuman</p>
-        <p>Opini</p>
-        <p>Kepustakaan</p>
-      </div>
-      <div className='mt-4 mb-16 grid grid-cols-3 gap-y-12 mx-10'>
-        {
-          news.map((data, index) => (
-            <div className='w-96 mx-auto space-y-2 bg-slate-50 p-6 rounded-md border-2' key={index}>
-              <p className='uppercase text-sm text-gray-600'>{data.type}</p>
-              <h2 className='capitalize text-xl font-semibold'>{data.title}</h2>
-              <p className='text-sm leading-5'>{data.description}</p>
-              <div className='flex justify-between'>
-                <Image src={assets.logoGmiDefault} className='w-10'></Image>
-                <p className='uppercase'>1 Min Read</p>
+      <div className='-mt-24 flex flex-col justify-center items-center'>
+        <div className='flex flex-wrap justify-evenly lg:justify-start text-sm lg:text-base space-x-4 lg:space-x-12 text-white ml-0'>
+          <p>Beranda</p>
+          <p>Berita</p>
+          <p>Pengumuman</p>
+          <p>Opini</p>
+          <p>Kepustakaan</p>
+        </div>
+        <div className='mt-8 mb-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-12 mx-20'>
+          {
+            news.map((data, index) => (
+              <div className='w-full mx-auto  bg-slate-50 p-6 rounded-md border-2' key={index}>
+                <p className='uppercase text-sm text-gray-600'>{data.type}</p>
+                <h2 className='capitalize text-xl font-semibold my-2'>{data.title}</h2>
+                <p className='text-sm leading-5 line-clamp-[8] my-5'>{data.description}</p>
+                <div className='flex justify-between'>
+                  <Image src={assets.logoGmiDefault} className='w-10'></Image>
+                  <p className='uppercase'>1 Min Read</p>
+                </div>
               </div>
-            </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   )
