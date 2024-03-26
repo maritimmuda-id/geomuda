8372658d0f39a94bb0bd13fca6635c0cfec0453c
anisetyowa <anisetyowa@gmail.com>
2024-03-26 10:43:08 +0700

    feat: detail catalog page

--- components/landing-page/DetailProduk/DetailProduk.jsx
+++ components/landing-page/DetailProduk/DetailProduk.jsx
@@ -1,19 +1,51 @@
-// import { useRouter } from "next/router";
-// import { usePathname } from "next/navigation";
+"use client";
+
+import { useRouter } from "next/navigation";
 import { produk } from "@/constanst/katalog-produk";
 
 const DetailProduk = () => {
-  // const router = usePathname();
-  // const { id } = router.query;
+  const router = useRouter();
+  const { id } = router.query || {};
+
+  if (!id) {
+    return <div>ID RAK ONOOOOO</div>;
+  }
+
+  const selectedProduk = produk.find((item) => item.id === id);
+
+  if (!selectedProduk) {
+    return <div>Produk tidak ditemukan</div>;
+  }
+
+  return (
+    <div className="font-xl px-16 py-24">
+      <h1>{selectedProduk.nama}</h1>
+      <p>Harga: {selectedProduk.harga}</p>
+      <p>Deskripsi: {selectedProduk.deskripsi}</p>
+    </div>
+  );
+};
+export async function getServerSideProps(context) {
+  const { id } = context.query;
 
   // Cari produk berdasarkan ID dari router query
-  // const selectedProduk = produk.find((item) => item.id == id);
+  const selectedProduk = produk.find((item) => item.id == id);
 
-  // Jika produk tidak ditemukan, tampilkan pesan bahwa produk tidak ada
-  // if (!selectedProduk) {
-  //   return <div>Produk tidak ditemukan</div>;
-  // }
-  return <div className="font-xl px-16 py-24">TEST</div>;
-};
+  // Jika produk tidak ditemukan, kembalikan props dengan pesan bahwa produk tidak ada
+  if (!selectedProduk) {
+    return {
+      props: {
+        notFound: true,
+      },
+    };
+  }
+
+  // Kembalikan props dengan detail produk
+  return {
+    props: {
+      selectedProduk,
+    },
+  };
+}
 
 export default DetailProduk;
