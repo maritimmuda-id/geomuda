fc68a645d2014f7d5505aee4d954ca8822483c5f
amorazl <alfhiyana11@gmail.com>
2024-03-28 12:01:57 +0700

    add price and desc product

--- components/landing-page/KatalogProduk/KatalogProduk.jsx
+++ components/landing-page/KatalogProduk/KatalogProduk.jsx
@@ -7,7 +7,7 @@ import { produk } from "@/constanst/katalog-produk";
 
 const KatalogProduk = () => {
   return (
-    <div className="px-16 py-32">
+    <div className="px-16 py-28">
       <div className="md:gap-y-18 grid grid-cols-1 gap-x-8 gap-y-10 px-2 md:grid-cols-2 md:gap-x-16 lg:grid-cols-4 lg:px-24">
         {produk.map((data, index) => (
           <div
@@ -24,14 +24,14 @@ const KatalogProduk = () => {
               <h6 className="mx-3 text-lg font-semibold md:text-xl">
                 {data.name}
               </h6>
-              <p className="mx-2 pb-3">{data.desc}</p>
+              <p className="mx-2 py-2">{data.price}</p>
               <div className="mt-5">
                 <Link href={`/katalog/detail/${data.id}`}>
                   <button
                     type="button"
                     className="w-full rounded-xl bg-gray-900 px-16 py-2 font-semibold text-white transition duration-0 hover:bg-gray-700 hover:duration-300"
                   >
-                    DETAIL
+                    LIHAT
                   </button>
                 </Link>
               </div>
--- constanst/katalog-produk.js
+++ constanst/katalog-produk.js
@@ -5,25 +5,28 @@ export const produk = [
     id: 1,
     image: assets.katalog1,
     name: "Totebag Maritim Muda",
-    desc: "Totebag ini berbahan kanvas premium, memiliki kain yang tebal sehingga dapat menahan beban yang berat, gambar yang ada pada totebag ini adalah kapal dan dicetak dengan print, harga dari totebag ini berkisar Rp20.000,00.",
+    price: "Rp. 20.000,00",
+    desc: "Totebag Maritim Muda ini dibuat dari bahan kanvas berkualitas yang tahan lama. Kanvas ini dipilih secara khusus untuk memberikan kekuatan dan ketahanan ekstra, sehingga Anda dapat membawa barang-barang Anda dengan percaya diri dan nyaman. Dengan kombinasi desain yang terinspirasi dari keindahan alam laut Indonesia yang indah, totebag ini tidak hanya stylish tetapi juga kokoh, menjadikannya pilihan yang sempurna untuk menemani aktivitas sehari-hari Anda.",
   },
   {
     id: 2,
     image: assets.katalog2,
     name: "Tumbler Maritim Muda",
-    desc: "Souvenir Produk Menarik",
+    price: "Rp. 20.000,00",
+    desc: "Tumblr kaca Maritim Muda adalah teman minum Anda yang ramah lingkungan. Dengan kapasitas 300ml dan desain yang ergonomis, tumblr ini nyaman untuk dibawa ke mana saja. Cocok untuk digunakan di kantor, sekolah, atau saat piknik di alam terbuka. Dibuat dari kaca berkualitas tinggi yang aman digunakan untuk minuman panas maupun dingin dan mudah dibersihkan, menjadikannya pilihan yang aman dan praktis untuk digunakan setiap hari.",
   },
   {
     id: 3,
     image: assets.katalog3,
     name: "Topi Maritim Muda",
-    desc: "Souvenir Produk Menarik",
+    price: "Rp. 20.000,00",
+    desc: "Topi Maritim Muda terbuat dari bahan kanvas yang tahan lama dan nyaman dipakai. Didesain dengan detail yang teliti dan jahitan yang kuat, topi ini akan tetap terlihat bagus meskipun digunakan dalam berbagai aktivitas outdoor. Dilengkapi dengan aksesori tambahan seperti logo Maritim Muda yang menambahkan kesan eksklusif dan autentik pada topi Anda.",
   },
   {
     id: 4,
     image: assets.katalog4,
     name: "Sticker Maritim Muda",
-    desc: "Souvenir Produk Menarik",
+    price: "Rp. 20.000,00",
+    desc: "Tambahkan sentuhan keajaiban laut pada barang-barang Anda dengan stiker kreatif dari Maritim Muda! Dibuat dari bahan vinyl berkualitas tinggi yang tahan air dan tahan lama. Setiap stiker ini memiliki desain yang unik, mulai dari hewan laut yang lucu dan komponen laut yang menarik. Cocok untuk digunakan sebagai hiasan pada laptop, botol minum, atau bahkan sebagai hadiah untuk teman-teman Anda.",
   },
-  
 ];
