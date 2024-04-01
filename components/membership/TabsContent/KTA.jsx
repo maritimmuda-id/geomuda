57e4a9f56a487fa999a701d5f97bbc4197cad0d7
SalsyaNA <salsya.nuralfienda.te20@mhsw.pnj.ac.id>
2024-04-01 15:10:42 +0700

    fix error

--- components/membership/TabsContent/KTA.jsx
+++ components/membership/TabsContent/KTA.jsx
@@ -1,24 +1,24 @@
-'use client';
+"use client";
 
 // ** Import Assests
-import assets from '@/assets/assets';
+import assets from "@/assets/assets";
 
 // ** Import Zustand
-import { usePhotoKTA } from '@/zustand/usePhotoKTA';
+import { usePhotoKTA } from "@/zustand/usePhotoKTA";
 
 // ** Import Other
-import Image from 'next/image';
-import { IoIosAdd } from 'react-icons/io';
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
-import { useRef, useState } from 'react';
-import { nikParser } from 'nik-parser';
-import Swal from 'sweetalert2';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useRouter } from 'next/navigation';
-import dayjs from 'dayjs';
-import localize from "dayjs/plugin/localizedFormat";;
-import QRCode from 'qrcode.react';
+import Image from "next/image";
+import { IoIosAdd } from "react-icons/io";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import { useRef, useState } from "react";
+import { nikParser } from "nik-parser";
+import Swal from "sweetalert2";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import dayjs from "dayjs";
+import localize from "dayjs/plugin/localizedFormat";
+import QRCode from "qrcode.react";
 
 const KTA = ({ user, kta }) => {
   const { user_metadata, id } = user;
@@ -45,18 +45,18 @@ const KTA = ({ user, kta }) => {
       setValid(true);
 
       Swal.fire({
-        title: 'Berhasil',
-        text: 'Nik Telah Valid',
-        icon: 'success',
+        title: "Berhasil",
+        text: "Nik Telah Valid",
+        icon: "success",
         confirmButtonColor: false,
         timer: 1500,
       });
     } else {
       setValid(false);
       Swal.fire({
-        title: 'Gagal',
-        text: 'Nik Tidak Valid',
-        icon: 'error',
+        title: "Gagal",
+        text: "Nik Tidak Valid",
+        icon: "error",
       });
     }
   };
@@ -64,19 +64,18 @@ const KTA = ({ user, kta }) => {
   const handleSubmit = async () => {
     setLoadingGenerate(true);
 
-
     if (valid) {
       const dateNow = dayjs();
-      const nextDate = dateNow.add(3, 'year');
+      const nextDate = dateNow.add(3, "year");
 
       const { error } = await supabase
-        .from('kta')
+        .from("kta")
         .upsert({
           nama: user_metadata.fullname,
           no_anggota: Math.round(Math.random() * 9999999999).toString(),
           organisasi_daerah: user_metadata.province,
-          start_date: dateNow.format('YYYY-MM-DD'),
-          end_date: nextDate.format('YYYY-MM-DD'),
+          start_date: dateNow.format("YYYY-MM-DD"),
+          end_date: nextDate.format("YYYY-MM-DD"),
           photo: linkImage,
           no_ktp: nik,
         })
@@ -84,27 +83,27 @@ const KTA = ({ user, kta }) => {
 
       if (!error) {
         Swal.fire({
-          title: 'Berhasil',
-          text: 'KTA Telah Berhasil Dibuat',
-          icon: 'success',
+          title: "Berhasil",
+          text: "KTA Telah Berhasil Dibuat",
+          icon: "success",
           timer: 1500,
           showConfirmButton: false,
         });
         setLoadingGenerate(false);
       } else {
         Swal.fire({
-          title: 'Gagal',
+          title: "Gagal",
           text: error.message,
-          icon: 'error',
+          icon: "error",
           timer: 1500,
           showConfirmButton: false,
         });
       }
     } else {
       Swal.fire({
-        title: 'Gagal',
-        text: 'Nik Tidak Valid',
-        icon: 'error',
+        title: "Gagal",
+        text: "Nik Tidak Valid",
+        icon: "error",
         timer: 1500,
         showConfirmButton: false,
       });
@@ -120,11 +119,7 @@ const KTA = ({ user, kta }) => {
     const input1 = pdfRefFront.current;
     const input2 = pdfRefBack.current;
 
-    
-    Promise.all([
-      html2canvas(input1),
-      html2canvas(input2)
-    ]).then((canvas) => {
+    Promise.all([html2canvas(input1), html2canvas(input2)]).then((canvas) => {
       const pdf = new jsPDF("portrait", "mm", "a4", true);
       const pdfWidth = pdf.internal.pageSize.getWidth();
       const pdfHeight = pdf.internal.pageSize.getHeight();
@@ -143,7 +138,14 @@ const KTA = ({ user, kta }) => {
         imgWidth * ratio,
         imgHeight * ratio
       );
-      pdf.addImage(canvas[1].toDataURL("image/png"), 'PNG', imgX2, imgY2, imgWidth * ratio, imgHeight * ratio);
+      pdf.addImage(
+        canvas[1].toDataURL("image/png"),
+        "PNG",
+        imgX2,
+        imgY2,
+        imgWidth * ratio,
+        imgHeight * ratio
+      );
       pdf.save(`kta-${user_metadata.fullname}.pdf`);
 
       setloading(false);
@@ -164,13 +166,22 @@ const KTA = ({ user, kta }) => {
             <Image
               alt="No Picture"
               className="w-56 aspect-[3/4] rounded-xl border-2 border-slate-200 object-cover object-center"
-              src={dataKta?.photo ? dataKta?.photo : !imageUpload ? assets.defaultImage : URL.createObjectURL(imageUpload)}
+              src={
+                dataKta?.photo
+                  ? dataKta?.photo
+                  : !imageUpload
+                  ? assets.defaultImage
+                  : URL.createObjectURL(imageUpload)
+              }
               width={500}
               height={500}
             />
 
             {!validate && (
-              <button className="w-9 aspect-square bg-white border-2 rounded-full absolute -bottom-3 -right-4" onClick={setKta}>
+              <button
+                className="w-9 aspect-square bg-white border-2 rounded-full absolute -bottom-3 -right-4"
+                onClick={setKta}
+              >
                 <IoIosAdd className="text-green-500 text-3xl mx-auto" />
               </button>
             )}
@@ -179,22 +190,44 @@ const KTA = ({ user, kta }) => {
           <div className="flex flex-col space-y-4 justify-between w-full">
             <div className="flex flex-col space-y-2 w-full">
               <label>Nama</label>
-              <input type="text" value={user_metadata.fullname} disabled className="border-2 border-[#00000066] px-3 py-[6px] p-1 rounded-lg w-full disabled:cursor-not-allowed" />
+              <input
+                type="text"
+                value={user_metadata.fullname}
+                disabled
+                className="border-2 border-[#00000066] px-3 py-[6px] p-1 rounded-lg w-full disabled:cursor-not-allowed"
+              />
             </div>
             <div className="flex flex-col space-y-2 w-full">
               <label>Nomor KTP</label>
               <div className="flex gap-x-6">
-                <input type="text" disabled={validate} value={nik} onChange={(e) => setNik(e.target.value)} className="border-2 border-[#00000066] px-3 py-[6px] p-1 rounded-lg w-full disabled:cursor-not-allowed" />
+                <input
+                  type="text"
+                  disabled={validate}
+                  value={nik}
+                  onChange={(e) => setNik(e.target.value)}
+                  className="border-2 border-[#00000066] px-3 py-[6px] p-1 rounded-lg w-full disabled:cursor-not-allowed"
+                />
 
                 {!validate && (
-                  <button onClick={handleCekNik} className="bg-[#7B2418] text-white p-1 rounded-lg w-1/5">
+                  <button
+                    onClick={handleCekNik}
+                    className="bg-[#7B2418] text-white p-1 rounded-lg w-1/5"
+                  >
                     Cek
                   </button>
                 )}
               </div>
             </div>
-            <button disabled={!valid || !imageUpload || validate} onClick={handleSubmit} className="bg-[#7B2418] text-white p-2 rounded-lg w-full disabled:cursor-not-allowed disabled:bg-gray-400">
-              {loadingGenerate ? 'Loading...' : validate ? 'KTA Telah Digenerate' : 'Buat KTA'}
+            <button
+              disabled={!valid || !imageUpload || validate}
+              onClick={handleSubmit}
+              className="bg-[#7B2418] text-white p-2 rounded-lg w-full disabled:cursor-not-allowed disabled:bg-gray-400"
+            >
+              {loadingGenerate
+                ? "Loading..."
+                : validate
+                ? "KTA Telah Digenerate"
+                : "Buat KTA"}
             </button>
           </div>
         </div>
@@ -210,13 +243,19 @@ const KTA = ({ user, kta }) => {
               <Image src={assets.bannerKta} />
 
               <div className="absolute top-10 right-7 space-y-2">
-                <p className="text-white font-bold text-2xl text-center">KARTU TANDA ANGGOTA</p>
+                <p className="text-white font-bold text-2xl text-center">
+                  KARTU TANDA ANGGOTA
+                </p>
 
                 <Image src={assets.logoKta} className="w-96 " />
               </div>
 
               <div className="bg-white w-56 rounded-full h-56 border-8 border-white absolute -bottom-28 left-10">
-                <Image src={dataKta.photo} className="overflow-hidden relative mx-auto rounded-full object-cover object-center" fill />
+                <Image
+                  src={dataKta.photo}
+                  className="overflow-hidden relative mx-auto rounded-full object-cover object-center"
+                  fill
+                />
               </div>
             </div>
 
@@ -227,57 +266,77 @@ const KTA = ({ user, kta }) => {
                   <h1 className="text-[#C93233] text-2xl">Anggota Geomuda</h1>
                 </div>
               </div>
-                  <h1 className="text-[#C93233] text-3xl">Anggota Biasa Geomuda</h1>
+            </div>
+
+            <div className="bg-white px-16 flex gap-10 pb-8">
+              <div>
+                {/* https://geomuda.id/user/${dataKta.no_anggota}/membership */}
+                <QRCode
+                  value={`http://localhost:3000/viewprofile/${
+                    dataKta.no_anggota
+                  }/${dataKta.nama}/${
+                    dataKta.organisasi_daerah
+                  }/${encodeURIComponent(dataKta.photo)}`}
+                  size={100}
+                />
+              </div>
+              <div className="flex flex-col gap-8">
+                <div className="space-y-1">
+                  <h1 className="font-semibold text-lg">No. Anggota</h1>
+                  <p className="text-lg">{dataKta.no_anggota}</p>
                 </div>
 
-              <div className="px-16 flex gap-10 pb-8">
-                <div>
-                  {/* https://geomuda.id/user/${dataKta.no_anggota}/membership */}
-                  <QRCode value={`http://localhost:3000/viewprofile/${dataKta.no_anggota}/${dataKta.nama}/${dataKta.organisasi_daerah}/${encodeURIComponent(dataKta.photo)}`} size={100} />
-                </div>
-                <div className="flex flex-col gap-8">
-                  <div className="space-y-1">
-                    <h1 className="font-semibold text-lg">No. Anggota</h1>
-                    <p className="text-lg">{dataKta.no_anggota}</p>
-                  </div>
-
-                  <div className="space-y-1">
-                    <h1 className="font-semibold text-lg">Organisasi Daerah</h1>
-                    <p className="text-lg">{dataKta.organisasi_daerah}</p>
-                  </div>
-                </div>
-
-                <div className="border w-[0.1 px] h-44 border-slate-500 "></div>
-
-                <div className="flex flex-col gap-8 me-10">
-                  <div className="space-y-1">
-                    <h1 className="font-semibold text-lg">Tanggal Berlaku</h1>
-                    <p className="text-lg">
-                      {dayjs(dataKta.start_date).format("LL")}
-                    </p>
-                  </div>
-
-                  <div className="space-y-1">
-                    <h1 className="font-semibold text-lg">Tanggal Berakhir</h1>
-                    <p className="text-lg">
-                      {dayjs(dataKta.end_date).format("LL")}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="mt-10 me-30">
-                  <Image src={assets.ttdKetuaUmum} className="w-28" />
+                <div className="space-y-1">
+                  <h1 className="font-semibold text-lg">Organisasi Daerah</h1>
+                  <p className="text-lg">{dataKta.organisasi_daerah}</p>
                 </div>
               </div>
+
+              <div className="border w-[0.1 px] h-44 border-slate-500 "></div>
+
+              <div className="flex flex-col gap-8 me-10">
+                <div className="space-y-1">
+                  <h1 className="font-semibold text-lg">Tanggal Berlaku</h1>
+                  <p className="text-lg">
+                    {dayjs(dataKta.start_date).format("LL")}
+                  </p>
+                </div>
+
+                <div className="space-y-1">
+                  <h1 className="font-semibold text-lg">Tanggal Berakhir</h1>
+                  <p className="text-lg">
+                    {dayjs(dataKta.end_date).format("LL")}
+                  </p>
+                </div>
+              </div>
+
+              <div className="mt-10 me-30">
+                <Image src={assets.ttdKetuaUmum} className="w-28" />
+              </div>
+            </div>
+
+            <div className="relative">
+              <Image src={assets.footerKta} />
             </div>
           </div>
 
-          <button
-            className="w-full md:w-[914px] mx-auto md:flex md:justify-center bg-[#7B2418] text-white py-2"
-            onClick={downloadPdf}
+          <div
+            ref={pdfRefBack}
+            className="absolute -mt-[9999px] md:relative md:-mt-0 w-[914px] h-[589px] mx-auto pt-5"
           >
-            {loading ? "Loading..." : "Simpan KTA"}
-          </button>
+            <div className="relative">
+              <Image src={assets.backKta} />
+            </div>
+          </div>
+
+          <div className="mt-5">
+            <button
+              className="w-full md:w-[914px] mx-auto md:flex md:justify-center bg-[#7B2418] text-white py-2"
+              onClick={downloadPdf}
+            >
+              {loading ? "Loading..." : "Simpan KTA"}
+            </button>
+          </div>
         </div>
       )}
     </>
