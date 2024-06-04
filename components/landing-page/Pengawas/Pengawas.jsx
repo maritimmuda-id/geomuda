480103b720cd63fd3d488179596b5526d9d35eda
anisetyowa <anisetyowa@gmail.com>
2024-06-04 10:57:41 +0700

    add pengawas

---
+++ components/landing-page/Pengawas/Pengawas.jsx
@@ -0,0 +1,33 @@
+// ** Import Next
+import Image from "next/image";
+
+// ** Import Constant
+import { advisor } from "@/constanst/pengawas";
+
+const Pengawas = () => {
+  return (
+    <div className="px-16 py-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 gap-x-8 md:gap-y-18 md:gap-x-16 px-2 lg:px-24">
+        {advisor.map((data, index) => (
+          <div
+            key={index}
+            className={`rounded-xl shadow-lg border-2 overflow-hidden w-full mx-auto`}
+          >
+            <Image
+              src={data.image}
+              priority
+              alt="Image profile"
+              className="mx-auto aspect-[3/4] w-full"
+            />
+            <div className="text-center py-6 space-y-2">
+              <h6 className="text-lg md:text-xl font-semibold">{data.name}</h6>
+              <p className="py-1">{data.position}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Pengawas;
---
+++ constanst/pengawas.js
@@ -0,0 +1,9 @@
+import assets from "@/assets/assets";
+
+export const advisor = [
+  {
+    image: assets.naufalImage,
+    name: "Muhammad Naufal Syafrun",
+    position: "Ketua Dewan Pengawas",
+  },
+];
